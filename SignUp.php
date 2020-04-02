606885dbd93fa4fd9d18d40accb536abf540a0fa
CarsonProkop <Carson_prokopuik@outlook.com>
2020-04-01 17:41:48 -0700

    Carson's Signup

---
+++ SignUp.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+
+<?php 
+	include 'header.php';
+
+
+
+?>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Sign up</title>
+		<link rel="stylesheet" href="CSS/SignUp.css">
+	</head>
+
+
+
+</html>
+
+
+
+<?php 
+	include 'footer.php';
+?>
